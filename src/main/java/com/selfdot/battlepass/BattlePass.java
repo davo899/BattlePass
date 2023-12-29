f0b35e8d2d2b581d5947085c25ff5620fdf37d9f
David Sadler <djs21@ic.ac.uk>
2023-12-29 19:31:30 +0000

    init

---
+++ src/main/java/com/selfdot/battlepass/BattlePass.java
@@ -0,0 +1,12 @@
+package com.selfdot.battlepass;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BattlePass implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
